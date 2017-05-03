981a9cf94a1417576d93d5697dbaa0bbac699793
bram <david.j.bramwell@googlemail.com>
2017-05-02 23:16:36 -0300

    Cleanup duplicated code in tests

--- src/test/java/com/example/LinkedListSorterTest.java
+++ src/test/java/com/example/LinkedListSorterTest.java
@@ -1,19 +1,26 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 import java.util.LinkedList;
 
 public class LinkedListSorterTest {
 
-    @Test public void testPutEvenNumbersToBack() {
-        LinkedListSorter sorter = new LinkedListSorter();
-        LinkedList<Integer> list = new LinkedList<Integer>();
+	LinkedListSorter sorter;
+    LinkedList<Integer> list;
+
+	@Before public void initialize() {
+        sorter = new LinkedListSorter();
+        list = new LinkedList<Integer>();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
+    }
+
+    @Test public void testPutEvenNumbersToBack() {
         LinkedList<Integer> sortedList = sorter.putEvenNumbersToBack(list);
         assertEquals((int) 1, (int) sortedList.get(0));
         assertEquals((int) 3, (int) sortedList.get(1));
@@ -23,13 +30,6 @@ public class LinkedListSorterTest {
     }
 
     @Test public void testPutOddNumbersToBack() {
-        LinkedListSorter sorter = new LinkedListSorter();
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
         LinkedList<Integer> sortedList = sorter.putOddNumbersToBack(list);
         assertEquals((int) 2, (int) sortedList.get(0));
         assertEquals((int) 4, (int) sortedList.get(1));
